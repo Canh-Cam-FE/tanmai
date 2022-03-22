c1471fa07ab14fad26e235ffbf4f64009bfc49ab
Lâm Trần <lamzaaa@yahoo.com>
2022-03-22 10:27:27 +0700

    up

--- _gulptasks/fullpage.js
+++ _gulptasks/fullpage.js
@@ -1,7 +1,4 @@
-import {
-	src,
-	dest
-} from "gulp";
+import { src, dest } from "gulp";
 import plumber from "gulp-plumber";
 import uglifyBabel from "gulp-terser";
 import babel from "gulp-babel";
@@ -11,12 +8,14 @@ import sourcemap from "gulp-sourcemaps";
 export const jsFullPageTask = () => {
 	return src(["src/js/fullpage.js"])
 		.pipe(plumber())
-		.pipe(babel({
-			presets: ["@babel/preset-env"]
-		}))
+		.pipe(
+			babel({
+				presets: ["@babel/preset-env"],
+			})
+		)
 		.pipe(uglifyBabel())
 		.pipe(rename("fullpage.min.js"))
-		.pipe(dest("dist/js"))
-}
+		.pipe(dest("dist/js"));
+};
 
 module.exports = jsFullPageTask;
